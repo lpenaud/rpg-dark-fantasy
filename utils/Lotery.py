f2a5ef760cd86c0af1eae3ef2e408ffdd88537d3
Unknown <erwan.bourhis29@gmail.com>
2017-10-12 20:33:22 +0200

    Update Lotery.py and stuff.json
    Finished data in Lotery.py and change the frenchname of item's rarity per english name.

--- utils/Lotery.py
+++ utils/Lotery.py
@@ -5,11 +5,46 @@ class Lotery(object):
     def __init__(self, strf):
         data = load(strf)
         self.__rarity = {
-            'commun': {
-                'items':data['commun'],
+            'common': {
+                'items':data['common'],
                 'options': {
                     'rarety': (215, 1000),
-                    'color':'gray'
+                    'color':'grey'
+                }
+            }
+            'unCommon': {
+                'items':data['unCommon'],
+                'options': {
+                    'rarety': (65, 214),
+                    'color':'green'
+                }
+            }
+            'rare': {
+                'items':data['rare'],
+                'options': {
+                    'rarety': (15, 64),
+                    'color':'blue'
+                }
+            }
+            'epic': {
+                'items':data['epic'],
+                'options': {
+                    'rarety': (5, 14),
+                    'color':'orange'
+                }
+            }
+            'legendary': {
+                'items':data['legendary'],
+                'options': {
+                    'rarety': (2, 4),
+                    'color':'red'
+                }
+            }
+            'mythical': {
+                'items':data['mythical'],
+                'options': {
+                    'rarety': (1, 1),
+                    'color':'purple'
                 }
             }
         }
@@ -24,7 +59,7 @@ class Lotery(object):
         return self.__rarity.copy()
 
     def loot(self):
-        randNum = rand.randint(215, 1000) # replace 215 by 0 when data is finish
+        randNum = rand.randint(1, 1000)
 
         for key, value in self.rarity.items():
             if randNum >= value['options']['rarety'][0] and randNum <= value['options']['rarety'][1]:
