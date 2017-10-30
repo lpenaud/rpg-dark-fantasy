e8c81e8c0014054e5228581c640260d757e2cbdd
Penaud Loïc <l.penaud@zaclys.net>
2017-10-30 23:11:02 +0100

    Add utils/setInterval.py
    This file apport a sort of equivalent of setInterval javascript function.
    In fact, a function is invoke every x seconds x times.

---
+++ utils/setInterval.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env/python3
+# coding: utf-8
+import threading
+
+class ThreadJob(threading.Thread):
+    def __init__(self,callback,interval,times):
+        '''runs the callback function after interval seconds
+
+        :param callback:  callback function to invoke
+        :type callback: function
+        :param interval: time in seconds after which are required to fire the callback
+        :type interval: float
+        :param time: number of times callback is invoke
+        :type time: int
+        '''
+
+        self.callback = callback
+        self.event = threading.Event()
+        self.interval = interval
+        self.times = times
+        self.currentTimes = 0
+        super(ThreadJob,self).__init__()
+
+    def run(self):
+        while not(self.event.wait(self.interval)) and self.currentTimes != self.times:
+            self.callback()
+            self.currentTimes += 1
